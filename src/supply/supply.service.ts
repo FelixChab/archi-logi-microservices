bb8df81bad96f999b13280b6e0fa75b5ac0b5bd4
felixchab <felixchabellard33@gmail.com>
2025-02-25 14:06:39 +0100

    impl issue #2 & minor fixes

--- src/supply/supply.service.ts
+++ src/supply/supply.service.ts
@@ -1,8 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { SupplyInputDto } from './supply.dto';
+import { SupplyInputDto, SupplyProductDto } from './supply.dto';
 import { StockMovementDto, StockMovementType } from '../dto/stockMovement.dto';
+import { ProductDto } from '../dto/product.dto';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class SupplyService {
@@ -14,18 +16,41 @@ export class SupplyService {
     return 'pong';
   }
   
+  // Issue #1 & #2 - Vérifie si un produit existe dans le catalogue
+  private async isProductInCatalog(productId: string): Promise<boolean> {
+    const catalog = await firstValueFrom(this.httpService.get<ProductDto[]>(`/api/products`));
+    return catalog.data.some(product => product.ean === productId);
+  }
+
+  // Issue #2 - Création d'un produit dans le catalogue
+  private async createProduct(product: SupplyProductDto): Promise<void> {
+    const newProduct: ProductDto = {
+      ean: product.ean,
+      name: product.name,
+      description: product.description,
+      categories: [],
+      price: product.purchasePricePerUnit,
+    };
+    await firstValueFrom(this.httpService.post(`/api/products`, newProduct));
+  }
+
   // Issue #1 - Gestion approvisionnement
   async handleSupply(input: SupplyInputDto): Promise<void> {
     for (const product of input.products) {
+      const productExists = await this.isProductInCatalog(product.ean)
+      // On vérifie si le produit existe dans le catalogue
+      if (!productExists) {
+        await this.createProduct(product);
+      }
       const stockMovement: StockMovementDto = {
         status: StockMovementType.Supply,
         productId: product.ean,
         quantity: product.quantity,
       };
       // Envoi l'incrément de stock
-      await this.httpService.post(`/api/stock/${input.supplyId}/movement`, stockMovement).toPromise();
+      await this.httpService.post(`/api/stock/${input.supplyId}/movement`, stockMovement);
       // Confirme la bonne réception de la commande
-      await this.httpService.post('http://microservices.tp.rjqu8633.odns.fr/api/supply-request', stockMovement).toPromise();
+      await this.httpService.post(`http://donoma.ddns.net/api/supply-request`, stockMovement);
     }
   }
 }
