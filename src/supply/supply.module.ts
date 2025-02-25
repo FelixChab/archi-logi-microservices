6fcb9c718ff978f79e2ca87efb5b4aea6face905
Hugo Lasserre <hugo.lasserrecab@gmail.com>
2025-02-25 14:29:36 +0100

    fix update supply module to include custom providers

--- src/supply/supply.module.ts
+++ src/supply/supply.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SupplyService } from './supply.service';
 import { SupplyController } from './supply.controller';
+import { supplyProviders } from './providers/supply.provider';
 
 @Module({
-  providers: [SupplyService],
+  providers: [...supplyProviders, SupplyService],
   controllers: [SupplyController],
 })
 export class SupplyModule {}
