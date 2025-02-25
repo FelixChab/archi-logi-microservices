1be8d26363ed6ff1a961ba762ae97c8945ff9aa4
Hugo Lasserre <hugo.lasserrecab@gmail.com>
2025-02-25 14:41:00 +0100

    feat: add ConfigModule import to supply module

--- src/supply/supply.module.ts
+++ src/supply/supply.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SupplyService } from './supply.service';
 import { SupplyController } from './supply.controller';
 import { supplyProviders } from './providers/supply.provider';
+import { ConfigModule } from '../config/config.module';
 
 @Module({
+  imports: [ConfigModule],
   providers: [...supplyProviders, SupplyService],
   controllers: [SupplyController],
 })
