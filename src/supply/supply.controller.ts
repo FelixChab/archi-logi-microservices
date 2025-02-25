95425f1e5e6ef4247e493d4ea55fd71341514f4f
tgentreau <gentreau.thomas@gmail.com>
2025-02-25 13:59:06 +0100

    feat: issue #4

--- src/supply/supply.controller.ts
+++ src/supply/supply.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { SupplyService } from './supply.service';
+import { RequiredSupplyDto } from './supply.dto';
 
 @Controller('supply')
 export class SupplyController {
@@ -8,5 +9,13 @@ export class SupplyController {
   @Get('ping')
   ping() {
     return this.supplyService.ping();
+  }
+
+  @Post('supply-needed')
+  @HttpCode(204)
+  async notifySuppliers(
+    @Body() requiredSupplyDto: RequiredSupplyDto,
+  ): Promise<void> {
+    await this.supplyService.notifySuppliers(requiredSupplyDto);
   }
 }
--- src/supply/supply.service.ts
+++ src/supply/supply.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { RequiredSupplyDto, SupplyRequestDto } from './supply.dto';
+import { ProductDto } from '../dto/product.dto';
 
 @Injectable()
 export class SupplyService {
-  public ping(): string {
+  ping(): string {
     return 'pong';
+  }
+
+  async notifySuppliers(requiredSupplyDto: RequiredSupplyDto): Promise<void> {
+    const productToSupply: ProductDto = await this.getProduct(
+      requiredSupplyDto.productId,
+    );
+    const supplyRequest: SupplyRequestDto = {
+      ean: productToSupply.ean,
+    };
+    await fetch('http://microservices.tp.rjqu8633.odns.fr/api/supply-request', {
+      method: 'POST',
+      body: JSON.stringify(supplyRequest),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  async getProduct(productId: string): Promise<ProductDto> {
+    const response = await fetch(
+      `http://microservices.tp.rjqu8633.odns.fr/api/products/${productId}`,
+    );
+    return await response.json();
   }
 }
